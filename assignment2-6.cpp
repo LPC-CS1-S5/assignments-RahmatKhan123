17c78f63c385a1e581b3d6cb86439aca9e65660a
Rahmatkhan <rahmatullahbkhrtr@gmail.com>
2021-02-08 20:46:18 +0000

    assignment 2-6

---
+++ assignment2-6.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a = 28, b = 32, c = 37, d = 24, e = 33;
+  int sum = a + b + c + d + e;
+  double avg = sum / 5;
+  cout << " the sum is: " << sum << " and the average is: " << avg << endl;
+}
