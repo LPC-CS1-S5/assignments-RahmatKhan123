c9115cb7dc9bfe489bde919bab16b3a5265ccdcb
Rahmatkhan <rahmatullahbkhrtr@gmail.com>
2021-02-25 21:19:58 +0000

    q44 is the elaboration of the quiz 4 quiestion2

---
+++ q44.cpp
@@ -0,0 +1,3 @@
+/*
+for this question i used the two if statement for establising the two rule of the company to mandate the range of delivery distance and weight ploicies.
+then i used the if else/if state with the or (||) and (&&) conjuction to check for the range of the conditions. 
