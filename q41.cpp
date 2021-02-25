3005c1d2d9444977cca8c650ecd8d077c6c3ad02
Rahmatkhan <rahmatullahbkhrtr@gmail.com>
2021-02-25 20:04:04 +0000

    just did it professor the right way.

--- q41.cpp
+++ q41.cpp
@@ -1,35 +1,93 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
   
-  float radius, area, lenght, width, base_lenght, height;
+  float radius, area, lenght, width, base_lenght, height,quit = 0;
   
   int choice;
-  if (choice = 1)
+  cout << "choose from the number 1, 2, 3, 4:" << endl;
+  cout << "1. Inpute to calculate the area of circle for you:" << endl;
+  cout << "2. Input to calculate the area of rectangle for you: " << endl;
+  cout << "3. Input to calculate the area of traingle for you:" << endl;
+  cin >> choice;
+    
+    
+  if(choice == 1)
   {
+
     cout << "Input the Raduis:" << radius << endl;
-    cin >> radius >> endl;
-    cout << area = 3.14 * radius * radius;
+    cin >> radius;
+    area = 3.14 * radius * radius;
+    cout << "area of cicle = " << area << endl;
   }
-  if (choice = 2)
+  
+  else if (choice == 2)
   {
-    cout << "input the lenght and width" << lenght << width << endl;
+    cout << "input the lenght and width" << endl;
     cin >> lenght >> width;
     area = lenght * width;
+    cout << "area of rectangle = " << area << endl;
   }
-  if (choice = 3)
+  else if (choice == 3)
   {
-    cout << "input the lenght of base and hieght:" << base_lenght << height << endl;
+    cout << "input the lenght of base and hieght:" << endl;
     cin >> base_lenght >> height;
+    area = base_lenght * height * 0.5;
+    cout << "area of traingle =" << area << endl;
   }
-  if (choice = 4)
+  else if (choice == 4)
   { 
     cout << "you number is ouside range:" << endl;
 
   }
-  if (choice < 0)
-  {
-    cout << "Error!";
-  }
+  else (choice < 0);
+      cout << "Error!";
+  
 }
+
+
+
+
+
+
+/*switch(choice)
+  {
+    case 1:
+      {
+        cout << "Input the radius of the circle:" << endl;
+        cin >> radius;
+        area = 3.14 * radius * radius;
+        cout << "area of circle is: " << area << endl;
+      break;
+      }
+
+      case 2:
+      {
+        cout << "Enter the lenght and width of of rectangle:" << endl;
+        cin >> lenght >> width;
+        area = lenght * width;
+        cout << "area of rectangle is" << area << endl;
+      break;
+      }
+
+      case 3:
+      {
+        cout << "Input the base-lenght and height of traingle" << endl;
+        cin >> base_lenght >> height;
+        area = base_lenght * height * 0.5;
+        cout << "are of traingle is" << area << endl;
+      break;
+      }
+
+      case 4:
+      exit(0);
+      }
+
+      default:
+      cout << "Invalide choice:";
+      }
+  }*/
+
+      
