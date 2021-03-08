d6fcd7deacd58b3a5c6dd8463fd9fd0dc4364d60
Rahmatkhan <rahmatullahbkhrtr@gmail.com>
2021-03-08 06:38:58 +0000

    i kind remeber but now i can't do it

---
+++ lab5-13.cpp
@@ -0,0 +1,18 @@
+/*Make a program that prints the index (i, j) with the given order in the blue triangle by using the nested for loop. The index will be printed by the left to right and the top to bottom.
+Reference the problem details to the lecture slides.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int i, j;
+  for (i = 1; i <= 1; i++)
+  {
+    for (j = 4; j <= 5; ++j)
+    {
+      cout << j << endl;
+      
+    } 
+    cout << i << endl;
+  }
+}
