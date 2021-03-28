1cbc7b17a1f5c5aafcfbe812ac3e2cac5672b802
Rahmatkhan <rahmatullahbkhrtr@gmail.com>
2021-03-28 02:33:46 +0000

    it is my Lab6-5 program

---
+++ lab6-5-func.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int oddsumget(int);
+int evensumget(int);
+
+int eversumget(int)
+{
+  static int eSum = 0;
+  Esum += Even;
+  return Esum;
+}
+
+int oddsumGet(int)
+{
+  static oSum = 0;
+  oSum += Odd;
+  return oSum;
+}
+
