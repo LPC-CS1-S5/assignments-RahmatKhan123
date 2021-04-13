03e1273db350e8368a5616d8bdc2029ef8afa44d
Rahmatkhan <>
2021-04-13 19:14:00 +0000

    I could not figure out why the "undefined reference to 'swapTwoValues ' function is. i beleive my code is the right way.

--- Quiz6/Qu1.cpp
+++ Quiz6/Qu1.cpp
@@ -11,34 +11,33 @@ void swapTwoValues3(float &, float &);
 
 int main()
 {
-  int n1, n2;
-  float f1, f2;
-  cout << "input first number: " << endl;
-  cin >> n2;
-  cout << "input the second number decimale: " << endl;
-  cin >> f2;
-  cout << "now i swapped them for you: " << endl;
+  int n1 =10 , n2 =20;
+  cout << "input first two number: " << endl;
+  cin >> n1>> n2;
+  swapTwoValues(n1, n2);
+  cout << "after swapt: " << n2 << " " << n1 << endl;
 
-
-  swapTwoValues(n1 , n2);
-
-  cout << "after swapt: " << n1 << " " << n2 << endl;
-
+  float f1 = 10.25, f2 = 20.20;
+  cout << "Input 2nd two decimale values: " << endl;
+  cin >> f1 >> f2;
   swapTwoValues3(f1 , f2);
 
-  cout << "After swap : " << n1 << " " << n2 << endl;
+  
+  cout << "After swap : " << f2 << " " << f1 << endl;
+  return 0;
 }
 
-void swapTwoValudes(int & n1, int & n2)
+void swapTwoValudes(int &n1, int &n2)
 {
   int temp;
   temp = n1;
   n1 = n2;
   n2 = temp;
+  cout << "in function: " << n1 << " " << n2 << endl;
 }
-void swapTwoValues3(float & f1, float & f2)
+void swapTwoValues3(float &f1, float& f2)
 {
-  int temp;
+  float temp;
   temp = f1;
   f1 = f2;
   f2 = temp;
