ecf7b4eb7fcf14081be89e18d557519ea5950f43
Rahmatkhan <>
2021-05-04 21:29:55 +0000

    trying to figure it out

--- midterm/Quiz7/1stQu.cpp
+++ midterm/Quiz7/1stQu.cpp
@@ -21,18 +21,15 @@ using namespace std;
 
 const int N = 10;
 
-//int checkZero(int []);
 int difference(int []);
 int difference1(int []);
 int difference2(int[]);
-//int checkLast(int[]);
+
 void printout(int []);
 
 int main()
 {
-  //int difference;
-  //int Zero;
-  //int Last;
+  
   int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
 
   //checkZero(number);
@@ -79,43 +76,23 @@ int difference2(int n[])
   {
     if ((i > 1) && (i < 8))
     {
-      if ((abs(n[i] - n[i - 1]) > 0 ) && (abs(n[i] - n[i+1]) < 10))
+      if ((abs(n[i-1] - n[i]) > 0 ) && (abs(n[i+1] - n[i]) < 10))
       {
-        cout << " the difference except el 1 to 9: is : " << n[i] - n[i-1] << " and " << n[i] - n[i+1] << endl;
-        cout << " the difference from el 1 to 8 is: " << n[i] << endl; 
+        cout << " the difference except el 1 to 9: is : " << n[i+1] - n[i] << " and " << n[i-1] - n[i] << endl;
       }
+      cout << " the difference from el 1 to 8 is: " << n[i] << endl; 
 
     }
     else 
       return 0;
   }
+
+
+
+
+  
 }
 
-/*
-
-    else if ( i == 9)
-    {
-      if(abs(n[i] - n[i-1]) < 10);{
-        cout << "the abs of last elemnt minus second to last element " << n[i-1] - n[i] << " is " << n[i] << endl;
-        cout << "the last element difference is: " << n[i] << endl;
-      }
-
-    }
-    else if (i > 1) 
-    {
-      if ((abs(n[i] - n[i - 1]) > 0 ) && (abs(n[i] - n[i+1]) < 10))
-      {
-        cout << " the difference except el 1 to 9: is : " << n[i] - n[i-1] << " and " << n[i] - n[i+1] << endl;
-        cout << " the difference from el 1 to 8 is: " << n[i] << endl; 
-      }
-
-    }
-    
-    
-
-  }
-}
-*/
 void printout(int n[])
 {
   for (int i = 0; i < N; i++)
@@ -124,55 +101,9 @@ void printout(int n[])
   
 }
 
-/*[Program Requirements]
-Define the function to check the element is satisfied with the difference condition.
-Make the for-loop and call the function N times to check all element values
-
-For the first and last element(i=0 or i=9), we will check just one adjacent value. For all other elements, we need to check the difference between the left and right adjacent elements.
-In order to get the absolute value of difference between elements, we use the standard library function abs( ) that is defined in <cmath>. For example, 
-if (abs(n[i] - n[i + 1]) < 10)
-	cout << “ less than 10\n”;
-*/
-
-
-/*int checkZero(int n[])
-{
-  int Zero;
-  for (int i = 0; i < N; i++)
-  {
-    if ((((abs(n[i] -  n[i+1]) > 0 )) && ((abs(n[i] - n[i+ 1])) < 10)))
-      Zero = n[i];
-  }
-  return Zero;
-}
-int checkLast(int n[])
-{
-  int last;
-  for (int i = 0; i < N; i++)
-  {
-    if ((abs(n[i = 8] - n[i+1]) <= 10))
-      last = n[i];
-  }
-  return last;
-}
-*/
 
 
 
 
-
-
-    
-    
-    /*if ((abs(n[i == 0] - n[i+1]) < 10) || (abs(n[i == 9] - n[i-9]) < 10))
-    {
-      cout << n[i] << " is Less than 10\n";
-    }
-    else if((abs(n[i] - n[i+1]) < 10) || (abs(n[i] - n[i-1]) < 10)){
-      cout << n[i] << "is less than 10\n";
-    }
-    else 
-      return 0;*/
-  
 
 
--- midterm/Quiz7/2ndQu.cpp
+++ midterm/Quiz7/2ndQu.cpp
@@ -6,20 +6,29 @@ Use the given main function template and print out these elements only with the 
 
 
 // Use this main function.
-#include    <iostream>
+*/
+#include <iostream>
 using namespace std;
 
 void printtriangle(int numbers[][3], int);
 int main()
 {
-    const int SIZE = 3;
-    int numbers[SIZE][SIZE] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };
+  const int SIZE = 3;
+  int numbers[SIZE][SIZE] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };
     
-    printtriangle(numbers, SIZE);
+  printtriangle(numbers, SIZE);
 }
 void printtriangle(int num[][3], int size)
 {
+  int triangle;
+  for (int i = 0; i < size-1; i++)
+  {
+    cout << num[i] << endl;
+    for (int j = i; j < size; i++)
+    cout << num[i][j];
+  }
+  cout << endl;
    
 }
-*/
+
 
