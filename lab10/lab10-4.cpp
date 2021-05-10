6f563b75129c481803b2cb426acb6cc347ec517d
Rahmatkhan <>
2021-05-10 19:33:40 +0000

    syntax Error.

---
+++ lab10/lab10-4.cpp
@@ -0,0 +1,12 @@
+/*4. Programming Lab 10-4
+We are going to make a program that implements the original standard library functions
+strlen()
+strcpy()
+strncmp()
+strstr()
+The functions that we are going to implement
+int    stringLength(const char source[ ]);
+void stringCopy(const char source[ ], char destination[ ]);
+void stringCompare(const char source[ ], const char destination[ ]);
+void stringSplit(const char source[ ], char destination[ ][ ], char delimiter);
+*/
